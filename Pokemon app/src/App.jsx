d3938e47fae92a3c2f225b9f87f0973d795f1c27
Gavin Yip <gavin.yip88@gmail.com>
2023-11-20 06:17:07 +0000

    number id

--- Pokemon app/src/App.jsx
+++ Pokemon app/src/App.jsx
@@ -1,8 +1,13 @@
-import { useState } from 'react'
-
+import { useState } from "react";
 
 function App() {
- 
+  // set state for initial pokemon id
+  const [id, setId] = useState(1);
+  // function to generate random number on click and update id
+  function handleClick() {
+    setId(Math.floor(Math.random() * 1000 + 1) + 1);
+  }
+  return <div>{id}</div>;
 }
 
-export default App
+export default App;
